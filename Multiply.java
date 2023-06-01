2cbf61ffbdeeec4e9a737c010fe6ed99851a98cc
Rakesh <rakeshmali1220@gmail.com>
2023-06-01 11:55:50 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Multiply {
+    public static void main(String args[]) {
+
+        int a = 5;
+        int b = 6;
+
+        System.out.println("Multiplication is "a * b);
+
+    }
+}
